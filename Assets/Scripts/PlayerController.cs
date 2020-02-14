62c84f0622248d8e62916c4cdfec7ddb5c49ab7b
Braulio Duarte <brauliod617@gmail.com>
2020-02-14 00:26:11 -0500

    create basic crappy starter movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,19 +9,33 @@ public class PlayerController : MonoBehaviour {
     //move character positions accordingly 
     //start character animation
     // Use this for initialization
+    private const string WALKING = "Walking";
+    private const string RUNNING = "Running";
+    private const string CROUCHING = "Crouching";
 
-
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
     private Rigidbody2D rb2d;
     public float test;
 
+    private Vector2 idlePosition;
+    private string previousAnimation;
+
+
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
-        	
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        //gets current position which is idle
+        idlePosition = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        previousAnimation = null;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         handleMovement();
+
 	}
 
     void handleMovement() {
@@ -35,5 +49,40 @@ public class PlayerController : MonoBehaviour {
         rb2d.AddForce(movement);
         rb2d.velocity = movement * 5;
         test = movement.x;
+        updateAnimator(moveHorizontal, movement);
     }
+
+    void updateAnimator(float moveHorizontal, Vector2 movement) {
+        if(animator == null) {
+            print("Error in PlayerController.updateAnimator\n");
+            return;
+        }
+
+        if(previousAnimation != null && movement == idlePosition) {
+            animator.SetBool(previousAnimation, false);
+            return;
+        }
+        //if player is moving forward
+        if(moveHorizontal > 0) { 
+            animator.SetBool(WALKING, true);
+            previousAnimation = WALKING;
+
+            //if sprite was flipped, unflip it
+            if (spriteRenderer.flipX == true)
+                spriteRenderer.flipX = false;
+
+        }else if(moveHorizontal < 0) {
+            animator.SetBool(WALKING, true);
+            previousAnimation = WALKING;
+
+            //if sprite was flipped, unflip it
+            if (spriteRenderer.flipX == false)
+                spriteRenderer.flipX = true;
+        }else {
+            animator.SetBool(WALKING, false);
+            previousAnimation = WALKING;
+        }
+
+    }
+
 }
