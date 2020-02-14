59ff981da021710f46af522dc196b40b96a4b020
Braulio Duarte <brauliod617@gmail.com>
2020-02-13 22:27:58 -0500

    create animation and animator controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+    //read user input
+    //if arrow keys pressed, 
+    //move character positions accordingly 
+    //start character animation
+    // Use this for initialization
+
+
+    private Rigidbody2D rb2d;
+    public float test;
+
 	void Start () {
-		
+        rb2d = GetComponent<Rigidbody2D>();
+        	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        handleMovement();
 	}
+
+    void handleMovement() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = 0.0f;
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        if (rb2d == null)
+            return;
+
+        rb2d.AddForce(movement);
+        rb2d.velocity = movement * 5;
+        test = movement.x;
+    }
 }
