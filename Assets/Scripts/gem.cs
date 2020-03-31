6646a8cacdb3251c8d0875368daa1631d88b02f6
ConnorO700 <connor700@gmail.com>
2020-03-31 17:11:02 -0400

    created gems

---
+++ Assets/Scripts/gem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gem : MonoBehaviour {
+
+    public int gemValue = 1; 
+    void OnTriggerEnter2D(Collider2D otherObj)
+    {
+        if (otherObj.gameObject.CompareTag("Player"))
+        {
+            scoreManager.instance.changeScore(gemValue); 
+        }
+    }
+}
